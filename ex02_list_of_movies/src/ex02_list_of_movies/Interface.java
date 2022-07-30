9c7fc86cb5274ba3a5e5676e5661612234793c86
josepmartorell <martorelljosep>
2022-07-30 14:11:57 +0200

    ex2 list of movies finished

--- ex02_list_of_movies/src/ex02_list_of_movies/Interface.java
+++ ex02_list_of_movies/src/ex02_list_of_movies/Interface.java
@@ -1,15 +1,21 @@
 package ex02_list_of_movies;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 
 public class Interface extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 
 
@@ -19,10 +25,48 @@ public class Interface extends JFrame {
 	public Interface() {
 		setTitle("LIST OF MOVIES");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 563);
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		
+		setComponents();
+
+	}
+	
+	public void setComponents() {
+		
+		JLabel lblNewLabel = new JLabel("Enter a title film");
+		lblNewLabel.setBounds(29, 57, 86, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(27, 82, 372, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setBounds(26, 113, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("Films");
+		lblNewLabel_1.setBounds(31, 219, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setBounds(28, 244, 371, 22);
+		contentPane.add(comboBox);
+		
+		btnNewButton.addActionListener(new ActionListener(){	
+			public void actionPerformed(ActionEvent e){
+                if (textField.getText().compareTo("")!=0)
+                {	
+                	comboBox.addItem(textField.getText());
+                	textField.setText("");
+                }
+			}
+		});	
+		
 	}
 
 }
